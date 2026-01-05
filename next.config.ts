876c1ba62117d2ad514a6083681fd6e83adce8c3
Vandhiyadevan <vandhiyadevansj@gmail.com>
2026-01-05 17:00:29 +0530

    Add serverExternalPackages for Turso in Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['@libsql/client', '@prisma/adapter-libsql'],
 };
 
 export default nextConfig;
